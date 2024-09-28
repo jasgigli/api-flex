c9be2ef2b8b3b50bfe0c1ec1ac09c58b2bfe8fe7
Jasgiigli <overview.jjj@gmail.com>
2024-09-27 22:37:04 -0700

    Update package.json for version 1.0.2

--- src/core/token.js
+++
@@ -1,9 +0,0 @@
-let token = "";
-
-export const setToken = (newToken) => {
-  token = newToken;
-};
-
-export const refreshTokenIfNeeded = async (token) => {
-  // Logic for refreshing token if expired
-};
